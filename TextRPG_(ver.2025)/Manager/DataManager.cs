f2b1e1fb930c82c6a4eed095d118e0448cabb031
LeeYoungSin <dldudtls100@gmail.com>
2025-07-11 10:57:12 +0900

    feat : DataManager 세팅 - static 선언, 상점아이템, 인벤토리아이템 리스트 생성

--- TextRPG_(ver.2025)/Manager/DataManager.cs
+++ TextRPG_(ver.2025)/Manager/DataManager.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace TextRPG__ver._2025_
 {
-    internal class DataManager
+    public static class DataManager
     {
+        public static List<Item> StoreItems { get; private set; } = null!;
+        public static List<Item> Inventory { get; private set; } = null!;
+        public static void Init()
+        {
+            StoreItems = new List<Item>();
+            Inventory = new List<Item>();
+        }
     }
 }
