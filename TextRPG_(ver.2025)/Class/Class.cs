6d0faa12396b6bd8a34cbb254dfeb4f4b4876b39
LeeYoungSin <dldudtls100@gmail.com>
2025-07-11 10:12:40 +0900

    feat : Class 부모클래스 능력치 세팅

--- TextRPG_(ver.2025)/Class/Class.cs
+++ TextRPG_(ver.2025)/Class/Class.cs
@@ -8,5 +8,33 @@ namespace TextRPG__ver._2025_
 {
     public abstract class Class
     {
+        protected GameManager gameManager;
+        protected Class(GameManager gm) => gameManager = gm;
+
+        //클래스 이름
+        public string? Name;
+
+        //체력
+        public int CurHP;
+        public int MaxHp;
+
+        //레벨
+        public int Level;
+
+        /*        //경험치
+                public int CurExp;
+                public int MaxExp;*/  // 경험치 시스템은 추후에 구현예정이 있다면 사용
+
+        //공격력
+        public int CurAP;
+        public int MaxAP;
+
+        //방어력
+        public int CurDP;
+        public int MaxDP;
+
+        // 골드
+        public int Gold;
+        public int CurGold;
     }
 }
