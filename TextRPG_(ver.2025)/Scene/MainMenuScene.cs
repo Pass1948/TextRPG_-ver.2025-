18f074c6c9311e8feb725a6e7f32f99a77bd3cc7
LeeYoungSin <dldudtls100@gmail.com>
2025-07-11 10:11:09 +0900

    feat : 메인메뉴 화면 구성

--- TextRPG_(ver.2025)/Scene/MainMenuScene.cs
+++ TextRPG_(ver.2025)/Scene/MainMenuScene.cs
@@ -13,11 +13,43 @@ namespace TextRPG__ver._2025_
         }
         public override void Render()
         {
-            throw new NotImplementedException();
+            Print("◎메인메뉴◎", ConsoleColor.DarkYellow);
+            Print("스파르타 텍스트 RPG에 오시걸 환영합니다");
+            Print("게임을 진행하실려면 1번을 입력하여 직업을 선택하여 주시기 바랍니다.\n");
+
+            Print(1, "직업선택하기", ConsoleColor.DarkCyan);
+            Print(0, "게임종료", ConsoleColor.Magenta);
+
+            Print("\n원하시는 행동을 입력해주세요");
+            Console.Write(">>");
         }
         public override void Update()
         {
-            throw new NotImplementedException();
+            string input = Console.ReadLine();
+
+            int index;
+            if (!int.TryParse(input, out index))
+            {
+                Console.WriteLine("\ninfo : 잘못 입력 하셨습니다.");
+                Thread.Sleep(1000); // 1초 대기
+                return;
+            }
+
+            switch (index)
+            {
+                case 1:
+                    gameManager.SwitchScene(SceneID.ClassSelect);
+                    Console.WriteLine("\ninfo : 게임을 시작합니다.");
+                    Thread.Sleep(1000);
+                    break;
+                case 0:
+                    gameManager.GameOver("\ninfo : 게임을 종료했습니다.");
+                    break;
+                default:
+                    Console.WriteLine("\ninfo : 잘못 입력 하셨습니다.");
+                    Thread.Sleep(1000);
+                    break;
+            }
         }
     }
 }
