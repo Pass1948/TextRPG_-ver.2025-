3ffcc05c3a43833ce23f7d9aaa7c55573fdb2297
LeeYoungSin <dldudtls100@gmail.com>
2025-07-11 10:13:47 +0900

    feat : 전사 클래스 초기 능력치 세팅

--- TextRPG_(ver.2025)/Class/Warrior.cs
+++ TextRPG_(ver.2025)/Class/Warrior.cs
@@ -8,5 +8,21 @@ namespace TextRPG__ver._2025_
 {
     public class Warrior : Class
     {
+        public Warrior(GameManager game) : base(game)
+        {
+            //클래스 이름
+            Name = "전사";
+            MaxHp = 200;
+            Level = 1;
+            MaxAP = 6;
+            MaxDP = 10;
+            Gold = 1000;
+
+            // 초기화
+            CurHP = MaxHp;
+            CurAP = MaxAP;
+            CurDP = MaxDP;
+            CurGold = Gold;
+        }
     }
 }
