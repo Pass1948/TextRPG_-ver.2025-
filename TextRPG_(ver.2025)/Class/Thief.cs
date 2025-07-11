04cc5bbd5ce1890b4c066241b4959fea75877d99
LeeYoungSin <dldudtls100@gmail.com>
2025-07-11 10:13:17 +0900

    feat : 도적 클래스 초기 능력치 세팅

--- TextRPG_(ver.2025)/Class/Thief.cs
+++ TextRPG_(ver.2025)/Class/Thief.cs
@@ -8,5 +8,21 @@ namespace TextRPG__ver._2025_
 {
     public class Thief : Class
     {
+        public Thief(GameManager game) : base(game)
+        {
+            //클래스 이름
+            Name = "도적";
+            MaxHp = 130;
+            Level = 1;
+            MaxAP = 10;
+            MaxDP = 5;
+            Gold = 1000;
+
+            // 초기화
+            CurHP = MaxHp;
+            CurAP = MaxAP;
+            CurDP = MaxDP;
+            CurGold = Gold;
+        }
     }
 }
