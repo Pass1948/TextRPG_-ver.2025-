863c91246521b6417bd44a42db12209f00774d4c
LeeYoungSin <dldudtls100@gmail.com>
2025-07-11 10:14:18 +0900

    feat : 마법사 클래스 초기 능력치 세팅

--- TextRPG_(ver.2025)/Class/Wizard.cs
+++ TextRPG_(ver.2025)/Class/Wizard.cs
@@ -8,5 +8,21 @@ namespace TextRPG__ver._2025_
 {
     public class Wizard : Class
     {
+        public Wizard(GameManager game) : base(game)
+        {
+            //클래스 이름
+            Name = "마법사";
+            MaxHp = 100;
+            Level = 1;
+            MaxAP = 10;
+            MaxDP = 3;
+            Gold = 1000;
+
+            // 초기화
+            CurHP = MaxHp;
+            CurAP = MaxAP;
+            CurDP = MaxDP;
+            CurGold = Gold;
+        }
     }
 }
